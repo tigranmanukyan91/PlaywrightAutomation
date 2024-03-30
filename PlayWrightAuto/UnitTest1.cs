3bec227a3cb27ce5306e8058578536533e3db319
Tigran <tmanukian91@gmail.com>
2024-03-30 21:57:32 +0400

    First commit

---
+++ PlayWrightAuto/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.Playwright;
+
+namespace PlayWrightAuto;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+    }
+}
